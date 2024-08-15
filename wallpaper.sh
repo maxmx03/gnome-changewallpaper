f1fba56446309637ad0194e4e427617a35471f52
Max Miliano <milianordelcanto@gmail.com>
2024-08-15 20:07:35 -0300

    fix: wallpaper list

--- wallpaper.sh
+++ wallpaper.sh
@@ -4,10 +4,10 @@ export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 declare wallpapers wallpaper total_wallpapers random_index
 
-wallpapers=($(ls "$HOME/Wallpapers"))
+wallpapers=($HOME/Wallpapers/*)
 total_wallpapers=${#wallpapers[@]}
 random_index=$((RANDOM % "$total_wallpapers"))
 wallpaper=${wallpapers["$random_index"]}
 
 gsettings set org.gnome.desktop.background picture-uri ""
-gsettings set org.gnome.desktop.background picture-uri-dark file://$HOME/Wallpapers/$wallpaper
+gsettings set org.gnome.desktop.background picture-uri-dark file://$wallpaper
