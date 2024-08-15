1c38f1550a7fa0d58f7b426e021c52b44f2f82d7
Max Miliano <milianordelcanto@gmail.com>
2024-08-15 10:09:22 -0300

    refactor: variables name and wallpaper folder

--- wallpaper.sh
+++ wallpaper.sh
@@ -2,13 +2,12 @@
 
 export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
-declare images random_index
+declare wallpapers wallpaper total_wallpapers random_index
 
-images=($(ls "$HOME/Imagens"))
-images_len=${#images[@]}
-random_index=$((RANDOM % "$images_len"))
-image=${images["$random_index"]}
+wallpapers=($(ls "$HOME/Wallpapers"))
+total_wallpapers=${#wallpapers[@]}
+random_index=$((RANDOM % "$total_wallpapers"))
+wallpaper=${wallpapers["$random_index"]}
 
 gsettings set org.gnome.desktop.background picture-uri ""
-gsettings set org.gnome.desktop.background picture-uri-dark file://$HOME/Imagens/$image
-
+gsettings set org.gnome.desktop.background picture-uri-dark file://$HOME/Wallpapers/$wallpaper
